ab47a0492a12ffd5e46c492bf3798b8937714c96
Hayden Banks <haydenbanks04@gmail.com>
2024-05-19 03:02:09 -0400

    Files added

---
+++ PokeGen.py
@@ -0,0 +1,81 @@
+from bs4 import BeautifulSoup
+import requests
+from datetime import date
+
+DATE = date.today()
+
+class File:
+
+    def __init__(self, file = "pokemon.csv"):
+        self.file = open(file, 'r')
+        self.links, self.names, self.quantity = [], [], []
+        for i in self.file:
+            i = i.strip().split(',')
+            self.names.append(i[0])
+            self.links.append(i[1])
+            self.quantity.append(i[2])
+
+    def __str__(self):
+        s = ''
+        for i in range(len(self.names)):
+            s += f'{self.names[i]}: {self.links[i]} [{self.quantity[i]}]\n'
+        return s
+
+class Page:
+
+    def __init__(self, url):
+        self.url = url
+        self.page = requests.get(url)
+        self.page = BeautifulSoup(self.page.text, 'html.parser')
+        self.attributes = self.organize(self.page)
+
+    def organize(self, page):
+        attributes = {
+                    'card': None,
+                    'price': None,
+                    'PSA-10': None
+                    }
+        if 'pricecharting' in self.url:
+            card = page.find('h1', attrs={'class':'chart_title'}).text.strip()
+            attributes['card'] = card
+
+            price = page.find('span', attrs={'class':'price js-price'}).text.strip()
+            attributes['price'] = price
+
+            psa10 = page.find('span', attrs={'title': 'current Manual Only value','class':'price js-price'}).text.strip()
+            attributes['PSA-10'] = psa10
+        elif 'trollandtoad' in self.url:
+            card = page.find('h1', attrs={'class':'font-weight-bold font-large font-md-largest mt-1 product-name'}).text.strip()
+            attributes['card'] = card
+
+            price = page.find('span', attrs={'id':'sale-price', 'class':'d-none'}).text.strip()
+            attributes['price'] = price
+
+            attributes['PSA-10'] = 'N/A'
+
+        return attributes
+
+def __main__():
+    file = File()
+    today = DATE
+    date = str(today.strftime('%m.%d.%y'))
+    newFile = open(f'PokeChart_{date}.csv', 'w')
+    newFile.write('card,ungraded,PSA 10,quantity\n')
+    sum = 0
+    psaSum = 0
+    for i in range(len(file.links)):
+        page = Page(file.links[i])
+        s = f'{file.names[i]},{page.attributes['price']},{page.attributes['PSA-10']},{file.quantity[i]}\n'
+        if '$' in page.attributes['price']:
+            sum += float(page.attributes['price'][1:])*float(file.quantity[i])
+        else:
+            sum += float(page.attributes['price'])*float(file.quantity[i])
+        if '$' in page.attributes['PSA-10']:
+            psaSum += float(page.attributes['PSA-10'][1:])*float(file.quantity[i])
+        newFile.write(s)
+        print(f'{file.names[i]}\nMarket: {page.attributes['price']}\nPSA-10: {page.attributes['PSA-10']}\n')
+    sum = round(sum, 2)
+    newFile.write(f'\n,,Collection Sum, PSA 10 Sum\n,,${sum},${psaSum}')
+
+if __name__ == '__main__':
+    __main__()
